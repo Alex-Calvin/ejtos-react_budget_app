c184a75a2932ae5304f76a784edf5ad8f6ba4e55
Alex Calvin <calvin.alex.j@gmail.com>
2024-05-14 12:05:54 -0400

    Saving progress.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,11 +27,24 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10
+        };
+
+        dispatch ({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>${props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button class="btn btn-success btn-circle rounded-circle" onClick={event=> increaseAllocation(props.name)}><FontAwesomeIcon icon={faChevronUp}/></button></td>
+        <td><button class="btn btn-danger btn-circle rounded-circle" onClick={event => decreaseAllocation(props.name)}><FontAwesomeIcon icon={faChevronDown}/></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
